54a3ec2cb655cda9fc8c0664a5291f2dec783207
dong <1044538798@qq.com>
2024-05-10 02:36:30 +0800

    modify

--- client/src/commands/touch.c
+++ client/src/commands/touch.c
@@ -3,8 +3,11 @@
 extern char currentChat[32];
 void printHeadline();
 void printLog();
+void clear();
+extern struct curPosition displayZoneStart, displayZoneEnd;
 void touch() {
   scanf("%32s", currentChat);
+  clear(displayZoneStart, displayZoneEnd);
   printHeadline();
   printLog();
 }
